179653168affbf3359afaf528ded81b7e2f61d3b
Michael Grande <michael.l.grande@gmail.com>
2013-10-13 13:48:31 -0400

    Outline of bi-objective ant colony optimization

---
+++ BiObjectiveAntColony.m
@@ -0,0 +1,26 @@
+function [ output_args ] = BiObjectiveAntColony( input_args )
+%BIOBJECTIVEANTCOLONY Summary of this function goes here
+%   Detailed explanation goes here
+
+%Outlining paper steps (Fig 2 - pg 1241)
+
+%Compute Second Heuristic Parameter
+
+%Create a Colony
+
+
+%Continue until end condition is satisfied
+endConditionMet = false;
+while ~endConditionMet
+    
+    
+    
+    
+    
+    
+end
+
+
+
+end
+
